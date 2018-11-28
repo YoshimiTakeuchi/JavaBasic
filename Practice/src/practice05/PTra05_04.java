28a80e1b02ebaa3f9c3840f58ceeff4f2c66207a
YoshimiTakeuchi <YoshimiTakeuchi@users.noreply.github.com>
2018-11-28 17:52:54 +0900

    ～5_4

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -22,17 +22,45 @@ public class PTra05_04 {
 		 *
 		 */
 
+		//①繰り返しON
+		boolean loopFlg = true;
+		while (loopFlg) {
+
 		// ①
-		System.out.println("0～6の整数を入力してください");
+			System.out.println("0～6の整数を入力してください");
 
-		String line = scanner.nextLine();
+			String line = scanner.nextLine();
 
-		switch(line) {
+			switch (line) {
 			case "0":
 				System.out.println("日曜");
+				loopFlg = false;//case～breakの間でループフラグをオフにする
 				break;
 			case "1":
 				System.out.println("月曜");
+				loopFlg = false;
+				break;
+			case "2":
+				System.out.println("火曜");
+				loopFlg = false;
+				break;
+			case "3":
+				System.out.println("水曜");
+				loopFlg = false;
+				break;
+			case "4":
+				System.out.println("木曜");
+				loopFlg = false;
+				break;
+			case "5":
+				System.out.println("金曜");
+				loopFlg = false;
+				break;
+			case "6":
+				System.out.println("土曜");
+				loopFlg = false;
+				break;
+			}
 		}
 	}
 }
